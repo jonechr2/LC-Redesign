cf6de7818ef86e03519d8e58b400f6620cd4211d
Chris <jonechr2@gmail.com>
2020-06-09 09:23:09 -0500

    .

---
+++ Content/Resources/Scripts/show-more.js
@@ -0,0 +1,23 @@
+﻿		$(document).ready(function(){
+			$(".menu:last").on("loaded", function(){
+				$('ul.nocontent.menu.mc-component').append('<div class="show more">More</div><div class="show less">Less</div>');
+				
+			var threshold = 4;
+			if ($('ul.nocontent.menu.mc-component').children().not('.show').length > threshold) {
+			$('.show.more').css('display', 'block');
+			}
+
+
+			$('.show.more').on('click', function() {
+			$(this).parent().children('li').not('.show').css('display', 'block');
+			$(this).parent().find('.show.less').css('display', 'block');
+			$(this).hide();
+			});
+
+			$('.show.less').on('click', function() {
+			$(this).parent().children(':nth-child(n+' + (threshold + 1) + ')').not('.show').hide();
+			$(this).parent().find('.show.more').css('display', 'block');
+			$(this).hide();
+			});
+			});
+			});
