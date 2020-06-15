d67c097f6fcb4317d84894eca39ca965f09956e9
Chris <jonechr2@gmail.com>
2020-06-15 11:17:35 -0500

    fixing script

--- Content/Resources/Scripts/show-more.js
+++ Content/Resources/Scripts/show-more.js
@@ -1,23 +1,28 @@
 ﻿		$(document).ready(function(){
-			$(".menu:last").on("loaded", function(){
+		$(".menu:last").on("loaded", function(){
 				$('ul.nocontent.menu.mc-component').append('<div class="show more">More</div><div class="show less">Less</div>');
 				
-			var threshold = 4;
-			if ($('ul.nocontent.menu.mc-component').children().not('.show').length > threshold) {
-			$('.show.more').css('display', 'block');
-			}
+				var threshold = 5;
+			 $('ul.nocontent.menu.mc-component').each(function(){
+						var $this = $(this);
+							if ($this.children().not('.show').length > threshold) {
+						
+							$this.children('.show.more').css('display', 'block');
+					}
+					});
+					
 
 
-			$('.show.more').on('click', function() {
-			$(this).parent().children('li').not('.show').css('display', 'block');
-			$(this).parent().find('.show.less').css('display', 'block');
-			$(this).hide();
-			});
+						$('.show.more').on('click', function() {
+						$(this).parent().children('li').not('.show').css('display', 'block');
+						$(this).parent().find('.show.less').css('display', 'block');
+						$(this).hide();
+					});
 
-			$('.show.less').on('click', function() {
-			$(this).parent().children(':nth-child(n+' + (threshold + 1) + ')').not('.show').hide();
-			$(this).parent().find('.show.more').css('display', 'block');
-			$(this).hide();
-			});
+				$('.show.less').on('click', function() {
+						$(this).parent().children(':nth-child(n+' + (threshold + 1) + ')').not('.show').hide();
+						$(this).parent().find('.show.more').css('display', 'block');
+						$(this).hide();
+					});
 			});
 			});
